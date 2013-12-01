f957f99c9e8912ed41c8dce484a71d5ad3e459fc
irbash <irbash@gmail.com>
2013-12-01 09:23:32 +0100

    Create GeoPosition.java

---
+++ org/goeuro/sample/jsonObject/GeoPosition.java
@@ -0,0 +1,31 @@
+package org.goeuro.sample.jsonObject;
+
+/*
+ * GeoPosition: Holds the content of geo position object inside the json object
+ */
+public class GeoPosition
+{
+    private float latitude;
+    private float longitude;
+
+    public float getLatitude()
+    {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude)
+    {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude()
+    {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude)
+    {
+        this.longitude = longitude;
+    }
+
+}
